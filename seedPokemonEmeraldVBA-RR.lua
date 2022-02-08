bc1642f90bffe2d13bfef207799229eaeff8654e
NPO <77460082+NPO-197@users.noreply.github.com>
2022-02-08 09:26:39 -0500

    Intial Commit

---
+++ seedPokemonEmeraldVBA-RR.lua
@@ -0,0 +1,44 @@
+
+ColorRed = 0xffa00000
+
+rshift = bit.rshift
+lshift = bit.lshift
+band = bit.band
+
+--Formula for LCRNG(R)
+function back(s)
+	local a = 0xEEB9 * band(s, 0xffff) + rshift(s, 16) * 0xEB65
+	local b = 0xEB65 * band(s, 0xffff) + band(a, 0xffff) * 0x10000 + 0xA3561A1
+	return b
+end
+
+--Reverses base seed until it encounters first 16bit seed
+function validationSeed(s)
+	local useed = s
+	while useed > 0xffff do
+		s = back(s)
+		useed = s
+		if s>0xffffffff then useed = s-0x100000000 end
+	end
+	return s
+end
+
+function Distance(s)
+  local i = 0
+	local useed = s
+  while useed> 0xffff do
+    i = i+1
+    s = back(s)
+		useed = s
+		if s>0xffffffff then useed = s-0x100000000 end
+  end
+  return i
+end
+
+while true do
+	seed = memory.readlong(0x03005D80);
+	gui.text( 0, 116, "RNG: "..string.format("%08X", seed))
+  gui.text( 0, 132, "Seed:"..string.format("%08X", validationSeed(seed)))
+	gui.text( 0, 148, "Dist:"..Distance(seed))
+	vba.frameadvance();
+end
